39ca29f0c46a9f50bf89a03d2d7a38b6690593b6
Tankobot <Tankobot@users.noreply.github.com>
2016-02-24 20:05:53 -0800

    Fix vector scalar multiplication.

--- Vector.py
+++ Vector.py
@@ -1,15 +1,17 @@
 class DimensionError(Exception):
     pass
 
+
 def int_str(x):
     return [str(i) for i in x]
+
 
 class Vector:
     def __init__(self, *args, extra=0):
         if extra:
             self.val = []
         else:
-            self.val = args
+            self.val = list(args)
         self.dim = len(args)
         for i in range(extra):
             self.val.append(0)
@@ -22,7 +24,7 @@ class Vector:
     
     # Vector Magnitude
     def mag(self):
-        return (i ** 2 for i in self.val) ** (1 / 2)
+        return sum([i ** 2 for i in self.val]) ** (1 / 2)
     
     # Dimension Check
     def __dim__(self, other):
@@ -35,36 +37,35 @@ class Vector:
     
     def __add__(self, other):
         self.__dim__(other)
-        newV = Vector(extra=self.dim)
+        new_v = Vector(extra=self.dim)
         for i in range(self.dim):
-            newV.val[i] = self.val[i] + other.val[i]
-        return newV
+            new_v.val[i] = self.val[i] + other.val[i]
+        return new_v
         
     def __sub__(self, other):
         self.__dim__(other)
-        newV = Vector(extra=self.dim)
+        new_v = Vector(extra=self.dim)
         for i in range(self.dim):
-            newV.val[i] = self.val[i] - other.val[i]
-        return newV
+            new_v.val[i] = self.val[i] - other.val[i]
+        return new_v
         
     def __mul__(self, other):
-        try:
-            other = float(other)
-            newS = Vector(extra=self.dim)
+        if type(other) in {int, float}:
+            new_s = Vector(extra=self.dim)
             for i in range(self.dim):
-                newS.val[i] = self.val[i] * other
-        except:
+                new_s.val[i] = self.val[i] * other
+        else:
             self.__dim__(other)
-            newS = 0
+            new_s = 0
             for i in range(self.dim):
-                newS += self.val[i] * other.val[i]
-        return newS
+                new_s += self.val[i] * other.val[i]
+        return new_s
         
     def __rmul__(self, other):
         return self.__mul__(other)
         
     # Common Vector Methods 
     
-    def proj(self, other):
+    def project(self, other):
         self.__dim__(other)
         return ((self * other) / other.mag() ** 2) * other
