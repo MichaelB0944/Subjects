9deb43bef74824e0ba9f33505d953fee36c69a19
MichaelB0944 <MichaelB0944@users.noreply.github.com>
2016-03-08 02:36:25 -0800

    Move chem file and add in structure for later.

---
+++ labpy/chem.py
@@ -0,0 +1,56 @@
+"""Perform chemistry."""
+
+import decimal
+from fractions import Fraction
+
+
+# --------------- #
+# Utility Objects #
+# --------------- #
+
+
+class Container:
+    pass
+
+
+# ------------------- #
+# Subatomic Particles #
+# ------------------- #
+
+
+class Proton:
+    pass
+
+
+class Neutron:
+    pass
+
+
+class Electron:
+    pass
+
+
+# ---------------- #
+# Atomic Particles #
+# ---------------- #
+
+
+class Element:
+    pass
+
+
+class Molecule:
+    pass
+
+
+# ------------- #
+# Other Mediums #
+# ------------- #
+
+
+class Substance:
+    pass
+
+
+class Mixture:
+    pass
