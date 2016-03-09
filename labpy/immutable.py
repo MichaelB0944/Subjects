d9ec8caa39e49fa0248248d013a626afb4d3fec5
MichaelB0944 <michaelb0944@gmail.com>
2016-03-09 13:32:04 -0800

    Create immutable.py

---
+++ labpy/immutable.py
@@ -0,0 +1,27 @@
+"""Subclass to create immutable objects.
+
+Example:
+    class Ex(Immutable):
+        @Ex.make_mutable
+        def __init__(self, name, count):
+            self.name = name
+            self.count = count
+"""
+
+class Immutable:
+    __mutable__ = True
+    def __setattr__(self, key, value):
+        if __mutable__:
+            super().__setattr__(key, value)
+        else:
+            raise AttributeError('object {0} is immutable'.format(repr(self)))
+    
+    @staticmethod
+    def make_mutable(f):
+        def mutable(*args, **kwargs):
+            super().__setattr__('__mutable__', True)
+            output = f(*args, **kwargs)
+            super().__setattr__('__mutable__', False)
+            
+            return output
+        return mutable
