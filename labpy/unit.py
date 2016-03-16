f226738d41192c8b2dd868dbf268a612d23ec3ec
MichaelB0944 <MichaelB0944@users.noreply.github.com>
2016-03-15 23:52:31 -0700

    Declare most of the standard scientific units.
    * Unit methods still need to be completed.

--- labpy/unit.py
+++ labpy/unit.py
@@ -1,49 +1,29 @@
 """Provide a class for describing units."""
 
 
-class Immutable:
-    """Class for immutable objects."""
-
-    __backup_setattr__ = object.__setattr__
-
-    @classmethod
-    def __unlock__(cls, self):
-        """Unlock attributes of instance.
-
-        Only use in class instantiation.
-        """
-
-        super(cls, self).__setattr__('__setattr__', super().__setattr__)
-
-    @classmethod
-    def __lock__(cls, self):
-        """Lock attributes of instance.
-
-        Only use in class instantiation.
-        """
-
-        super(cls, self).__setattr__('__setattr__', cls.__backup_setattr__)
-
-    @staticmethod
-    def wrap(f):
-        def g(self, *args, **kwargs):
-            self.__unlock__(self)
-            f(*args, **kwargs)
-            self.__lock__(self)
-        return g
-
-
-# ----- #
-# Units #
-# ----- #
-
-
-class Unit(Immutable):
+class Unit:
     """Abstract class for units."""
 
-    @Immutable.wrap
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, name='Unit', base=None, full_name=None, parent=None):
+        # Unlock object for initialization
+        temp_setattr = self.__setattr__
+        super().__setattr__('__setattr__', super().__setattr__)
+
+        self.__name__ = name
+        self.full_name = full_name or name
+        self.parent = parent
+
+        if base is None:
+            self.composition = None
+            self.power = None
+        elif isinstance(base, Unit):
+            self.composition = base.composition
+            self.power = base.power
+        else:
+            raise TypeError('expected Unit got %s' % repr(type(base)))
+
+        # Lock object after initialization
+        self.__setattr__ = temp_setattr
 
     def __setattr__(self, key, value):
         raise AttributeError('object Unit is immutable')
@@ -56,11 +36,63 @@ class Unit(Immutable):
     __rmul__ = __mul__
 
     def __truediv__(self, other):
-        """Separate unit from fraction."""
+        """Combine values of different or the same units."""
 
         pass
 
     def __rtruediv__(self, other):
-        """Separate unit from fraction."""
-
         return Unit.__truediv__(other, self)
+
+    def __pow__(self, power, modulo=None):
+        pass
+
+
+# ----- #
+# Units #
+# ----- #
+
+"Units gathered from www.wikibooks.org/wiki/General_Chemistry/Units" \
+    "Unit information available under CC Attribution-ShareAlike license"
+
+# SI Fundamental Units
+l = Unit('l', full_name='Length')
+t = Unit('t', full_name='Time')
+m = Unit('m', full_name='Mass')
+T = Unit('T', full_name='Thermodynamic Temperature')
+n = Unit('n', full_name='Amount of Substance')
+Q = Unit('Q', full_name='Electrical Charge')
+
+vol = Unit('vol', full_name='Volume')
+press = Unit('press', full_name='Pressure')
+
+# SI Fundamental Units (Unit Symbols)
+meter = Unit('meter', full_name='Meter', parent=l)
+s = Unit('s', full_name='Second', parent=t)
+kg = Unit('kg', full_name='Kilogram', parent=m)
+K = Unit('K', full_name='Kelvin', parent=T)
+mol = Unit('mol', full_name='Mole', parent=n)
+C = Unit('C', full_name='Coulomb', parent=Q)
+
+# SI Derived Units
+N = Unit('N', kg * meter / (s ** 2), full_name='Newton')
+J = Unit('J', kg * (meter ** 2) / (s ** 2), full_name='Joule')
+Pa = Unit('Pa', kg / meter / (s ** 2), full_name='Pascal')
+W = Unit('W', kg * (meter ** 2) / (s ** 3), full_name='Watt')
+A = Unit('A', C / s, full_name='Ampere')
+V = Unit('V', J / C, full_name='Volt')
+
+
+class UsefulUnit:
+    """Other Useful Units"""
+    L = Unit('L', full_name='Liter', parent=vol)
+    atm = Unit('atm', full_name='Atmosphere', parent=press)
+
+
+class SolutionUnit:
+    """solution Concentration Units"""
+    M = Unit('M', mol / UsefulUnit.L, full_name='Molarity')
+    m = Unit('m', mol / kg, full_name='Molality')
+    X = Unit('X', full_name='Mole Fraction')
+    ppm = Unit('ppm', full_name='Parts per Million')
+    ppb = Unit('ppb', full_name='Parts per Billion')
+
