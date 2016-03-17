cd461a64760cfce4bfc4977fd9d4512204b3aba8
Tankobot <Tankobot@users.noreply.github.com>
2016-03-17 01:11:54 -0700

    Create definitions for SigFig class special math methods.

--- labpy/sigfig.py
+++ labpy/sigfig.py
@@ -5,4 +5,42 @@ from decimal import *
 
 
 class SigFig:
-    pass
+    """A decimal that follows significant figure rules.
+
+    Attributes:
+    """
+
+    @staticmethod
+    def then_setattr(self):
+        raise AttributeError('SigFig is immutable')
+
+    def __init__(self, value=Decimal()):
+        self.context = Context(prec=len(str(value))-1)
+        self.value = self.context.create_decimal(value)
+        self.visual = lambda: '%.{0}e'.format(len(value.__str__()) - 2) % value
+        self.__setattr__ = self.then_setattr
+
+    def __add__(self, other):
+        pass
+
+    __radd__ = __add__
+
+    def __sub__(self, other):
+        pass
+
+    def __rsub__(self, other):
+        pass
+
+    def __mul__(self, other):
+        pass
+
+    __rmul__ = __mul__
+
+    def __truediv__(self, other):
+        pass
+
+    def __rtruediv__(self, other):
+        pass
+
+    def __pow__(self, power, modulo=None):
+        pass
